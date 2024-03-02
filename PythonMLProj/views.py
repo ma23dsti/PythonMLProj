510f69bf98802a0b8f04d5dc0eab06e5773861ea
Scientist DATA <marius.akre@edu.dsti.institute>
2024-03-02 22:46:35 +0100

    Update the response displayed to the end user

--- PythonMLProj/views.py
+++ PythonMLProj/views.py
@@ -29,11 +29,14 @@ def predict_ratings_view(request):
 
             #Transform input data and predict
             book_info_df_trans = transformer.transform(book_info_df)
-            temp = classifier.predict_proba(book_info_df_trans)
+            #book_predict_proba = classifier.predict_proba(book_info_df_trans)
             book_rating_pred = classifier.predict(book_info_df_trans)
-
+            if (book_rating_pred == 1):
+                rep = "High rate"
+            else:
+                rep = "Low rate"
             # Return results to the view to be displayed
-            return render(request, 'result-ratings.html', {'num_pages': num_pages, 'text_reviews_count': text_reviews_count, 'publicationYear': publicationYear, 'language_code_grouped': language_code_grouped, 'author_number_books': author_number_books, 'predicted_book_rating': ["prob",temp,"rate",book_rating_pred]})
+            return render(request, 'result-ratings.html', {'num_pages': num_pages, 'text_reviews_count': text_reviews_count, 'publicationYear': publicationYear, 'language_code_grouped': language_code_grouped, 'author_number_books': author_number_books, 'predicted_book_rating': rep})
     return render(request, 'predict_ratings.html', {'form': form})
 
 def home_view(request):
